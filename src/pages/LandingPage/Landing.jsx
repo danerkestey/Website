90de780df4debb043f5ab4e9827a0cc88fd16594
Daner Yasin <77366878+danerkestey@users.noreply.github.com>
2022-10-11 22:09:23 -0400

    cool changes

--- src/pages/LandingPage/Landing.jsx
+++ src/pages/LandingPage/Landing.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
+import Styled from 'styled-components'
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
+
+const StyledLanding = Styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #172A3A;
+    min-height: 100vh;
+    min-width: 100vw;
+`
+
+const SocialBar = Styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    gap: 1em;
+`
 
 const Landing = () => {
-  
     return (
-        <div>
+        <StyledLanding>
             <h1>new site under construction lol</h1>
-            <h2><a href="https://www.linkedin.com/in/daneryasin/">follow me on linkedin btw</a></h2>
-        </div>
-        
+            <SocialBar>
+                <a href="https://www.linkedin.com/in/daneryasin/" target="_blank" rel="noreferrer">
+                    <FaLinkedinIn size="4em" color="white"/>
+                </a>
+                <a href="https://github.com/danerkestey" target="_blank" rel="noreferrer">
+                    <FaGithub size="4em" color="white"/>
+                </a>
+            </SocialBar>
+        </StyledLanding>
     );
 };
   
