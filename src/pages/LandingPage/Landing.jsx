d8d46c6d98cb93e34ac1bdb4c3449c1880890c8f
Daner Yasin <danerkyasin@gmail.com>
2023-08-07 22:02:53 -0400

    change nice

--- src/pages/LandingPage/Landing.jsx
+++ src/pages/LandingPage/Landing.jsx
@@ -1,37 +1,63 @@
 import React from 'react'
 import Styled from 'styled-components'
+import content from './Content.json'
 import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
 
-const StyledLanding = Styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    background-color: #172A3A;
+const StyledApp = Styled.div`
     min-height: 100vh;
     min-width: 100vw;
+    background-color: #14213D;
+
+`
+
+const StyledNav = Styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    justify-content: left;
+    padding: 5em 0 2em 0;
 `
 
 const SocialBar = Styled.div`
     display: flex;
     flex-direction: row;
-    justify-content: space-around;
+    justify-content: center;
     gap: 1em;
+`
+
+const StyledDescription = Styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 10em 25em 0 25em;
 `
 
 const Landing = () => {
     return (
-        <StyledLanding>
-            <h1>new site under construction lol</h1>
+        <StyledApp>
+            <StyledDescription>
+                <StyledNav>
+                    <h1>{content.name}</h1>
+                    
+                </StyledNav>
+
+                <h2>{content.intro}</h2>
+                <p>{content.description}</p>
+                <p>{content.leadership}</p>
+                <p>{content.hobbies}</p>
+                <p>{content.outro}</p>
+            
+            </StyledDescription>
+
             <SocialBar>
                 <a href="https://www.linkedin.com/in/daneryasin/" target="_blank" rel="noreferrer">
-                    <FaLinkedinIn size="4em" color="white"/>
+                    <FaLinkedinIn size="2em" color="white"/>
                 </a>
                 <a href="https://github.com/danerkestey" target="_blank" rel="noreferrer">
-                    <FaGithub size="4em" color="white"/>
+                    <FaGithub size="2em" color="white"/>
                 </a>
             </SocialBar>
-        </StyledLanding>
+        </StyledApp>
+
     );
 };
   
